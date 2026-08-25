cf67f888cf2fd047c6097ee03adbfebdc5d80f34
Anurag Thakur <thakuranurag4788@gmail.com>
2026-08-25 23:15:05 +0530

    Enchance the UI Design

--- src/components/brands/BrandCard.jsx
+++ src/components/brands/BrandCard.jsx
@@ -5,25 +5,37 @@ export default function BrandCard({ brand }) {
     return (
         <article className={styles.card}>
 
+            {/* LOGO */}
+
             <div className={styles.logoWrapper}>
 
                 {brand.logo ? (
+
                     <img
                         src={brand.logo}
                         alt={`${brand.name} logo`}
                         className={styles.logo}
                     />
+
                 ) : (
+
                     <div className={styles.noLogo}>
-                        {brand.name.charAt(0)}
+                        {brand.name?.charAt(0)?.toUpperCase()}
                     </div>
+
                 )}
 
             </div>
 
+
+            {/* BRAND NAME */}
+
             <h3 className={styles.name}>
                 {brand.name}
             </h3>
+
+
+            {/* VIEW BUTTON */}
 
             <Link
                 href={`/brands/${brand.slug}`}
